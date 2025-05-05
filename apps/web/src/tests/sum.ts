508cb459b0cb0f29aea2bda3d02d44bc1d3e80b7
Kevin Djabaku Ocansey <kevinocansey11@gmail.com>
2025-05-06 00:00:50 +0100

    tests added

---
+++ apps/web/src/tests/sum.ts
@@ -0,0 +1,9 @@
+/**
+ * Adds two numbers together
+ * @param a First number
+ * @param b Second number
+ * @returns Sum of the two numbers
+ */
+export function sum(a: number, b: number): number {
+  return a + b;
+}
