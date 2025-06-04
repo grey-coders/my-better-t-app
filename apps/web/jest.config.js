086d83c11e7ca11f57ea5b025cb453bd83f83872
Kevin Djabaku Ocansey <kevinocansey11@gmail.com>
2025-06-04 19:24:46 +0100

    configuration files

---
+++ apps/web/jest.config.js
@@ -0,0 +1,7 @@
+export const preset = 'ts-jest';
+export const testEnvironment = 'jsdom';
+export const transform = {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+};
+export const extensionsToTreatAsEsm = ['.ts'];
+  
